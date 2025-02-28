06eb8bffab23b6607566f8d1ed352998c87d474b
Lukasz Nogaj <lukanogaj@hotmail.co.uk  git config --global user.name lukanogaj>
2025-02-28 10:45:35 +0000

    html-structure

--- script.js
+++ script.js
@@ -5,10 +5,34 @@ console.log(mainDiv);
 // Function to create HTML elements
 
 function createHTML(element, className, idName) {
-	const htmEl = document.createHTML(element);
+	const htmEl = document.createElement(element);
 	htmEl.classList.add(className);
 	if (idName) {
-		htmEl.setAttribute("id"), idName;
+		htmEl.setAttribute("id", idName);
 	}
 	return htmEl;
 }
+
+// Creating 3 mains containers for the project and nested them inside the container
+// Wrapper with navbar , hero, floating-icons
+const wrapper = createHTML("div", "wrapper", "wrap");
+//Add element into container
+mainDiv.appendChild(wrapper);
+// Division with the services elements
+const division = createHTML("div", "division", "division-services");
+//Add element into container
+mainDiv.appendChild(division);
+// Section with order card and footer
+const section = createHTML("div", "section", "sections");
+mainDiv.appendChild(section);
+
+// Wrapper with the navbar , which include left and right hand side of the header
+// Navbar
+const navbar = createHTML("div", "navbar", "header");
+wrapper.appendChild(navbar);
+// Navbar left with cv and copy button
+const navbarStart = createHTML("div", "navbar-start", "navStart");
+navbar.appendChild(navbarStart);
+// Navbar right with social links
+const navbarEnd = createHTML("div", "navbar-end", "navEnd");
+navbar.appendChild(navbarEnd);
