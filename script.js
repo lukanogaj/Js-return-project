c68067b31315dc78c330aba9b6bfeae12430a6f2
Lukasz Nogaj <lukanogaj@hotmail.co.uk  git config --global user.name lukanogaj>
2025-03-03 08:03:50 +0000

    map-method

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-import { navbarStartData } from "./data/data";
-console.log(navbarStartData);
+// import { navbarStartData } from "./data/data";
+// console.log(navbarStartData);
 
 // Selecting the Html elements
 const mainDiv = document.querySelector("#main-div");
 console.log(mainDiv);
-
+// Array with objects to use the data
+const data = {
+	dataNavbarStart: [
+		{
+			email: "lukanogaj@hotmail.co.uk",
+			copy: "Copy",
+			cv: "CV",
+		},
+	],
+	dataNavbarEnd: [
+		{
+			linkedin: "Linkedin",
+			slash: "/",
+			dribble: "Dribbble",
+			slash: "/",
+			instagram: "Instagram",
+		},
+	],
+};
 // Function to create HTML elements
-
 function createHTML(element, className, idName) {
 	const htmEl = document.createElement(element);
 	htmEl.classList.add(className);
@@ -15,9 +32,40 @@ function createHTML(element, className, idName) {
 	}
 	return htmEl;
 }
+//  Maps to create the start and end navbar
+// const navbarStartItems = data.dataNavbarStart.map((item) => {
+// 	const listStartItems = document.createElement("li");
+// 	listStartItems.setAttribute("id", "listItem");
+// 	listStartItems.classList.add("list-items");
+// 	navbar.appendChild(listStartItems);
+// 	listStartItems.innerHTML = item;
+// 	return item;
+// });
+// console.log(navbarStartItems);
 
 // Creating 3 mains containers for the project and nested them inside the container
-// Wrapper with navbar , hero, floating-icons
+const navbar = createHTML("div", "navbar", "header");
+// Entire navbar
+mainDiv.appendChild(navbar);
+
+// Navbar left with cv and copy button
+const navbarStart = createHTML("div", "navbar-start", "navStart");
+navbar.appendChild(navbarStart);
+//  Maps to create the start and end navbar
+const navbarStartItems = data.dataNavbarStart.map((item) => {
+	const listStartItems = document.createElement("li");
+	listStartItems.setAttribute("id", "listItem");
+	listStartItems.classList.add("list-items");
+	navbarStart.appendChild(listStartItems);
+	// listStartItems.innerHTML = item;
+	return item;
+});
+console.log(navbarStartItems);
+
+// Navbar right with social links
+const navbarEnd = createHTML("div", "navbar-end", "navEnd");
+navbar.appendChild(navbarEnd);
+// Wrapper, hero, floating-icons
 const wrapper = createHTML("div", "wrapper", "wrap");
 //Add element into container
 mainDiv.appendChild(wrapper);
@@ -29,13 +77,4 @@ mainDiv.appendChild(division);
 const section = createHTML("div", "section", "sections");
 mainDiv.appendChild(section);
 
-// Wrapper with the navbar , which include left and right hand side of the header
-// Navbar
-const navbar = createHTML("div", "navbar", "header");
-wrapper.appendChild(navbar);
-// Navbar left with cv and copy button
-const navbarStart = createHTML("div", "navbar-start", "navStart");
-navbar.appendChild(navbarStart);
-// Navbar right with social links
-const navbarEnd = createHTML("div", "navbar-end", "navEnd");
-navbar.appendChild(navbarEnd);
+// Wrapper  , which include left and right hand side of the header
